5f3aa340687671d73ce981631f2f66d43fd06499
= <49078581+walase01@users.noreply.github.com>
2022-02-14 08:58:18 -0400

    adding tab pages
    adding tab pages

--- FitYou/src/app/app.module.ts
+++ FitYou/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterpagesComponent } from './shared/footerpages/footerpages.component';
+import { TabpagesComponent } from './shared/tabpages/tabpages.component';
+
+FooterpagesComponent
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabpagesComponent,
+    FooterpagesComponent
   ],
   imports: [
     BrowserModule
---
+++ FitYou/src/app/shared/tabpages/tabpages.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabpages',
+  templateUrl: './tabpages.component.html',
+  styleUrls: ['./tabpages.component.css']
+})
+export class TabpagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
